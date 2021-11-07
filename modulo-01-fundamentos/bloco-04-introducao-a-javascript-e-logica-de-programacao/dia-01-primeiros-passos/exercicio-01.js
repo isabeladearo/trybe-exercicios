8c6f433b940aaf044c31143000eddcef0065fd1e
Isabela de Aro <euisabeladearo@gmail.com>
2021-11-07 15:46:44 -0300

    add exercise 01

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-primeiros-passos/exercicio-01.js
@@ -0,0 +1,19 @@
+//01. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para: 
+
+const a = 5;
+const b = 2;
+
+// Adição (a + b):
+console.log(a + b);
+
+// Subtração (a - b):
+console.log(a - b);
+
+// Multiplicação (a * b):
+console.log(a * b);
+
+// Divisão (a / b):
+console.log(a / b);
+
+// Módulo (a % b):
+console.log(a % b);
