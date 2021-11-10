9087b0b884d0e02b0e444875a791980e770a6827
Isabela de Aro <euisabeladearo@gmail.com>
2021-11-10 17:59:28 -0300

    add exercises 01/02/03/04/05/06

---
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/parte-01-objetos-e-for-in/exercicio-02.js
@@ -0,0 +1,23 @@
+//02. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console: 👇🏻
+
+// {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   }
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info.recorrente = 'Sim';
+
+for (let key in info) {
+    console.log(key + ': ' + info[key]);
+}
+
+
