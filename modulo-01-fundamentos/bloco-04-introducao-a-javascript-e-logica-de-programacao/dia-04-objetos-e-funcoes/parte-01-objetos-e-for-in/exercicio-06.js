cd297d93649572c7c14c95952a325791ded75093
Isabela de Aro <euisabeladearo@gmail.com>
2021-11-10 18:16:17 -0300

    update exercise 06

--- modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/parte-01-objetos-e-for-in/exercicio-06.js
+++ modulo-01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/parte-01-objetos-e-for-in/exercicio-06.js
@@ -4,11 +4,13 @@ let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
     idade: 21,
-    livrosFavoritos: {
+    livrosFavoritos: [
+      {
         titulo: 'O Pior Dia de Todos',
         autor: 'Daniela Kopsch',
         editora: 'Tordesilhas',
       },
+    ],
   };
 
-  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor.livrosFavoritos.titulo + "'."); 
+  console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + "'" + leitor.livrosFavoritos[0].titulo + "'."); 
