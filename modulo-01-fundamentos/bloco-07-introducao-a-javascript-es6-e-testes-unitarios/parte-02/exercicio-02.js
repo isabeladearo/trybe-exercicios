0fbc1837743aa6676a376d363c260202390d7b3d
Isabela de Aro <euisabeladearo@gmail.com>
2021-12-07 11:11:34 -0300

    Atualizando

--- modulo-01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/parte-02/exercicio-02.js
+++
@@ -1,9 +0,0 @@
-// Crie uma função que receba uma frase e retorne qual a maior palavra.
-
-const longestWord = string => {
-  const splitString = string.split(' ');
-  const sortString = splitString.sort((a, b) => (b.length) - a.length);
-  return sortString[0];
-}
-
-console.log(longestWord(string));
