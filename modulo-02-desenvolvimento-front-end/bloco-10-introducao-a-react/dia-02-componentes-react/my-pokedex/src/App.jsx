9d25425ff8a9bdbe24f95850979d113255b4885f
Isabela de Aro <euisabeladearo@gmail.com>
2022-01-25 11:03:51 -0300

    correcao formatacao

--- modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.jsx
+++ modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import data from './data';
+import React from "react";
+import data from "./data";
 import PokemonCard from "./PokemonCard";
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='title'>Pokedex</h1>
-        <div className='layout-page'>
-        { data.map((item) => <PokemonCard key={item.id} itemInfo={item} />) }
+        <h1 className="title">Pokedex</h1>
+        <div className="layout-page">
+          {data.map((item) => (
+            <PokemonCard key={item.id} itemInfo={item} />
+          ))}
         </div>
       </div>
-      
-    )
-  };
-};
+    );
+  }
+}
 
 export default App;
--- modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/PokemonCard.jsx
+++ modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/PokemonCard.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
-import './PokemonCard.css';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./PokemonCard.css";
+import PropTypes from "prop-types";
 
 class PokemonCard extends React.Component {
-    render() {
+  render() {
+    const {
+      itemInfo: {
+        name,
+        type,
+        averageWeight: { value, measurementUnit },
+        image,
+      },
+    } = this.props;
 
-        const { itemInfo: { name, type, averageWeight: { value, measurementUnit }, image }} = this.props;
-
-        return (
-            <div className='card-box'>
-                <div className='info-card'>
-                    <p>{name}</p>
-                    <p>{type}</p>
-                    <p>{value} {measurementUnit}</p>
-                </div>
-                <div className='img-card'>
-                    <img src={image} alt='Pokemon'/>
-                </div>
-            </div>
-        );
-    };
-};
+    return (
+      <div className="card-box">
+        <div className="info-card">
+          <p>{name}</p>
+          <p>{type}</p>
+          <p>
+            {value} {measurementUnit}
+          </p>
+        </div>
+        <div className="img-card">
+          <img src={image} alt="Pokemon" />
+        </div>
+      </div>
+    );
+  }
+}
 
 PokemonCard.propTypes = {
-    itemInfo: PropTypes.shape({
-        name: PropTypes.string,
-        type: PropTypes.string,
-        averageWeight: PropTypes.shape({
-            value: PropTypes.number,
-            measurementUnit: PropTypes.string,
-        }),
-        image: PropTypes.string,
-    }).isRequired,
-    
-}
+  itemInfo: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    }),
+    image: PropTypes.string,
+  }).isRequired,
+};
 
 export default PokemonCard;
